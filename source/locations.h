bd99980260ffc2a440ab49d76f13c33e1a56e54c
martin-kudlicka <martink-dev@yandex.com>
2021-04-12 20:31:05 +0200

    * use MSettingsGroup

--- source/locations.h
+++ source/locations.h
@@ -3,7 +3,7 @@
 
 #include "location.h"
 
-class Locations
+class Locations : public MSettingsGroup<Location>
 {
   public:
     struct Property
@@ -11,18 +11,8 @@ class Locations
       static const QString Group;
     };
 
-    Locations();
-
-    quintptr     count      ()                   const;
-    LocationSPtr get        (const MUuidPtr &id) const;
-    LocationSPtr get        (quintptr index)     const;
-    MUuidPtr     id         (quintptr index)     const;
-    quintptr     index      (const MUuidPtr &id) const;
-    bool         isEmpty    ()                   const;
-    void         removeIndex(quintptr index);
-
-  private:
-    QSettings _settings;
+             Locations();
+    virtual ~Locations() Q_DECL_OVERRIDE Q_DECL_EQ_DEFAULT;
 };
 
 #endif
